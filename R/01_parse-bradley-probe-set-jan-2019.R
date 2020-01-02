e2fdf5f1c74ad325e3323b4dc51947782c584c0d
Daniel Kein <daniel.kein@canada.ca>
2020-01-02 20:58:19 +0000

    Update 01_parse-bradley-probe-set-jan-2019.R

--- R/01_parse-bradley-probe-set-jan-2019.R
+++ R/01_parse-bradley-probe-set-jan-2019.R
@@ -322,7 +322,7 @@ report <-
 report <- 
   report %>% 
   mutate(
-    Mykrobe_Workflow_Version = params["Mykrobe_Pipeline_Version"],
+    Mykrobe_Pipeline_Version = params["Mykrobe_Pipeline_Version"],
     Mykrobe_min_depth = params["Mykrobe_min_depth"],
     Mykrobe_min_conf = params["Mykrobe_min_conf"],
     Mutation_set_version= params["Mutation_set_version"]
